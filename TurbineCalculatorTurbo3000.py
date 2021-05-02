2762d5aeb0c44b4ed96da040f79435b043f5b25b
Thiago de Sousa <thiagoburgani@gmail.com>
2021-05-01 23:51:30 -0500

    Added TurbineCalc for historical data

---
+++ TurbineCalculatorTurbo3000.py
@@ -0,0 +1,127 @@
+# Copyright Thiago de Sousa, Ashan Ran, and Juan Barbarena
+# under the MIT license
+
+# Turbine Calculator Turbo 3000 v1: the ultimate turbine calculator with
+# historical weather data
+
+# Required installed packages:
+    # pandas, geopy, windpowerlib, geocoder, meteostat, datetime, 
+    # timezonefinderL, matplotlib
+
+# Initial process: user input on desired location, timeframe and daily/hourly
+
+# import necessary packages
+# Data packages
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Geo packages
+from geopy.distance import geodesic
+import geocoder
+
+# Forecast packages
+import datetime
+from timezonefinderL import TimezoneFinder as TZFind
+from meteostat import Point, Hourly
+
+# User inputs: 
+desired_loc = str(input('Desired location: '))
+start_year = input('Start Year: ')
+start_month = input('Month (as number 1-12): ')
+start_day = input('Day: ')
+end_year = input('End Year: ')
+end_month = input('Month (as number 1-12): ')
+end_day = input('Day: ')
+
+try:
+    start_year,start_month,start_day = eval(start_year), eval(start_month), eval(start_day)
+    end_year,end_month,end_day = eval(end_year), eval(end_month), eval(end_day)
+except NameError:
+    print('Invalid input. Please try again')
+    exit()
+
+# Handling too old data and users from the future that don't know 
+# the gregorian calendar
+
+# Basic ones first
+if start_year < 2000 or start_year > end_year or not isinstance(start_year,int):
+    start_year = 2000
+    print('Warning: Fetching the data will likely take a long time')
+if (end_year > datetime.datetime.now().year or not isinstance(end_year,int)
+    or end_year < start_year):
+    end_year = datetime.datetime.now().year
+if not 1<=start_month<=12 or not isinstance(start_month,int):
+    start_month = 1
+if not 1<=end_month<=12 or not isinstance(end_month,int):
+    end_month = 1
+if not 1<=start_day<=31 or not isinstance(start_day,int):
+    start_day = 1
+if not 1<=end_day<=31 or not isinstance(end_day,int):
+    end_day = 1
+    
+# Big brain ones
+if start_year == end_year and start_month>end_month:
+    start_month = end_month
+if start_year == end_year and start_month == end_month:
+    if start_day > end_day:
+        start_day = end_day - 1
+        print('Warning: erroneous dates input. Single day will be obtained')
+
+if end_year == datetime.datetime.now().year:
+    if end_month > datetime.datetime.now().month:
+        end_month = datetime.datetime.now().month
+    if end_month == datetime.datetime.now().month:
+        if end_day > datetime.datetime.now().day:
+            end_day = datetime.datetime.now().day
+    
+
+   
+# Handling location
+Location = geocoder.osm(desired_loc)
+desired_lat_long = Location.latlng
+lat, long = desired_lat_long[0], desired_lat_long[1]
+
+# Determining where user is, to warn if input is very far from them
+user_current_loc = geocoder.ip('me')
+user_lat_long = user_current_loc.latlng
+distance_mi = round(geodesic(user_lat_long,desired_lat_long).miles,2)
+
+# Handling bad user input - location
+if isinstance(desired_lat_long,list) is False:
+    print('Invalid location, please try again')
+    exit()
+    distance_mi = 0
+
+if distance_mi > 350:
+    print('Warning! Your distance to the location is', distance_mi,'miles',
+          '\n','Consider refining your input')
+    YesorNo = input('Would you like to proceed? (Y/N): ')
+    if YesorNo != 'Y':
+        quit()
+
+# Handling start year, end year, and standardized timezone
+tf = TZFind()
+tz = tf.timezone_at(lng=long, lat=lat)
+start = datetime.datetime(start_year,start_month,start_day)
+end = datetime.datetime(end_year,end_month,end_day)
+
+# Obtaining weather data
+desired_point = Point(lat,long,0)
+weather_data = Hourly(desired_point, start, end)   
+weather_data = weather_data.fetch()
+
+# Manipulating to the format needed
+# Weather columns that aren't needed
+weather_data = weather_data.drop(['dwpt','rhum','prcp','snow','coco',
+                                  'tsun','wdir','wpgt'],axis=1)
+weather_data.rename(columns= {'temp':'temperature',
+                              'pres':'pressure',
+                              'wspd':'wind_speed'},inplace=True)
+
+# Unit conversion
+
+
+
+
+
+
